57269d1704817bf3aad3b94fee2a2f39d82e21cf
unker <kfi.unknown@gmail.com>
2023-08-06 15:21:32 +0300

    e2e passed

--- e2e/e2e.test.js
+++ e2e/e2e.test.js
@@ -1,4 +1,3 @@
-import { fork } from 'child_process';
 import puppetteer from 'puppeteer';
 
 jest.setTimeout(30000); // default puppeteer timeout
@@ -6,34 +5,55 @@ jest.setTimeout(30000); // default puppeteer timeout
 describe('Credit Card Validator form', () => {
   let browser = null;
   let page = null;
-  let server = null;
   const baseUrl = 'http://localhost:9000';
 
   beforeAll(async () => {
-    server = fork(`${__dirname}/e2e.server.js`);
-    await new Promise((resolve, reject) => {
-      server.on('error', reject);
-      server.on('message', (message) => {
-        if (message === 'ok') {
-          resolve();
-        }
-      });
-    });
-
     browser = await puppetteer.launch({
-      // headless: false, // show gui
-      // slowMo: 250,
-      // devtools: true, // show devTools
+      headless: false, // show gui
+      slowMo: 50,
+      devtools: true, // show devTools
     });
     page = await browser.newPage();
   });
 
   afterAll(async () => {
     await browser.close();
-    server.kill();
+    // server.kill();
   });
 
-  test('should add do something', async () => {
+  test('should be true if card numbre', async () => {
     await page.goto(baseUrl);
+
+    const form = await page.$('.card-form-widget');
+    const input = await form.$('.input');
+    const submit = await form.$('.submit');
+
+    await input.type('4111111111111111');
+    await submit.click();
+
+    const el = await input.getProperty('className');
+    let classList = await el.jsonValue();
+    classList = classList.split(' ');
+
+    expect(classList.includes('invalid')).toBe(false);
+    expect(classList.includes('valid')).toBe(true);
+  });
+
+  test('should be false if card numbre', async () => {
+    await page.goto(baseUrl);
+
+    const form = await page.$('.card-form-widget');
+    const input = await form.$('.input');
+    const submit = await form.$('.submit');
+
+    await input.type('4111111111111112');
+    await submit.click();
+
+    const el = await input.getProperty('className');
+    let classList = await el.jsonValue();
+    classList = classList.split(' ');
+
+    expect(classList.includes('invalid')).toBe(true);
+    expect(classList.includes('valid')).toBe(false);
   });
 });
